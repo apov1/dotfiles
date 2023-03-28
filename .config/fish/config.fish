8e31a8bdf58cce65b7fa35687f1bfc04012fbaff
apov1 <10307477+apov1@users.noreply.github.com>
2023-03-28 23:32:07 +0200

    Modify fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,11 @@
-starship init fish | source
+export LANG=en_US.UTF-8
+export editor='vim'
+
+# Initialize brew paths for macOS
+if test (uname) = Darwin
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+if status is-interactive
+    starship init fish | source
+end
